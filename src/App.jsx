8e41d27b0db8ecca1e95c41c79cd9750b3f42c44
Jim124 <fengpingan065@gmail.com>
2025-05-01 17:58:33 +0100

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { About } from './pages';
 function App() {
   return <>Hello world</>;
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,4 @@
+const About = () => {
+  return <h1 className='text-2xl'>About</h1>;
+};
+export default About;
