246bae64312708008ba40c9e50f075631405379d
Jim124 <fengpingan065@gmail.com>
2025-05-01 18:20:24 +0100

    modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,67 @@
-import { About } from './pages';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: HomeLayout,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        Component: Landing,
+      },
+      {
+        path: 'about',
+        Component: About,
+      },
+      {
+        path: 'products',
+        Component: Products,
+      },
+      {
+        path: 'products/:id',
+        Component: SingleProduct,
+      },
+      {
+        path: 'orders',
+        Component: Orders,
+      },
+      {
+        path: 'checkout',
+        Component: Checkout,
+      },
+      {
+        path: 'cart',
+        Component: Cart,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    Component: Login,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    Component: Register,
+    errorElement: <Error />,
+  },
+]);
 function App() {
-  return <>Hello world</>;
+  return <RouterProvider router={router}>Hello world</RouterProvider>;
 }
 
 export default App;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,13 @@
+import { Outlet } from 'react-router';
+
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <>
+      <nav>
+        <span>comfy</span>
+      </nav>
+      <Outlet />
+    </>
+  );
 };
 export default HomeLayout;
